43e616d657e89bbcf802fdc3e91a29e960363c71
MrWisest <harold.laurent@hotmail.com>
2018-05-05 09:31:09 +0200

    Add full project

---
+++ Adventurer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Adventurer
+{
+    class Program
+    {
+        static void Main()
+        {
+            string CharacterName = "John Doe";
+
+            Console.WriteLine("Askeer Bay");
+            Console.WriteLine("Welcome to the bay of Askeer. You will be exploring the town and discover many things");
+            Console.WriteLine("What should we call you ?");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Owh, that's a first. Right, we'll call you " + CharacterName);
+            Console.Read();
+        }
+    }
+}
